8653c878f70fd908f4bdc5aa3a4210bdb2c5de0a
kingyj00 <onshopyj@gmail.com>
2024-11-19 16:42:53 +0900

    App class

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,29 +3,24 @@ package com.ll;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args){
         App app = new App();
         app.run();
     }
 }
 
-class App{
-    public void run() {
-        System.out.println("==명언 앱==");
+class App {
+    public void run(){
+        System.out.println("== 명언 앱 ==");
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.print("명령) ");
+            System.out.print("명령 : ");
+
             String cmd = scanner.nextLine();
-
-            if (cmd.equals( "종료")) {
-                break;
-            }
-
-            System.out.println("입력된 명령어 : %s".formatted(cmd));
+            if (cmd.equals("종료")) break;
         }
-
         scanner.close();
     }
 }
