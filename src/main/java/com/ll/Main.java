d6d5035df0254e649fa2606ec6ad9d56a1d9488b
kingyj00 <onshopyj@gmail.com>
2024-11-20 16:26:01 +0900

    Step2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,24 +3,27 @@ package com.ll;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         App app = new App();
         app.run();
     }
 }
-
 class App {
     public void run() {
-        System.out.println("==명언 앱==");
+        System.out.println("== 명언 앱 ==");
 
         Scanner scanner = new Scanner(System.in);
 
-        while (true){
+        while (true) {
             System.out.print("명령 : ");
-
             String cmd = scanner.nextLine();
             if (cmd.equals("종료")) break;
+            else if (cmd.equals("등록"))
+                System.out.println("명언 : ");
+                String content = scanner.nextLine();
+                System.out.println("작가 : ");
+                String author = scanner.nextLine();
         }
         scanner.close();
         }
-}
+    }
