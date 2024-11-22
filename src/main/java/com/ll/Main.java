523b5074365d269bf9dd6050da5880a554b616b8
kingyj00 <onshopyj@gmail.com>
2024-11-22 11:11:04 +0900

    Step6-Class33-divide

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -9,34 +9,20 @@ public class Main {
     }
 }
 class App {
+    Scanner scanner = new Scanner(System.in);
+    int lastId = 0;
+    WiseSaying[] wiseSayings = new WiseSaying[10];
+    int wiseSayingSize = 0;
+
     public void run() {
         System.out.println("== 명언 앱 ==");
-
-        Scanner scanner = new Scanner(System.in);
-        int lastId = 0;
-
-        WiseSaying[] wiseSayings = new WiseSaying[10];
-        int wiseSayingSize = 0;
 
         while (true) {
             System.out.print("명령 : ");
             String cmd = scanner.nextLine();
             if (cmd.equals("종료")) break;
             else if (cmd.equals("등록")) {
-                System.out.println("명언 : ");
-                String content = scanner.nextLine();
-                System.out.println("작가 : ");
-                String author = scanner.nextLine();
-                int id = ++lastId;
-
-                WiseSaying wiseSaying = new WiseSaying(id, content, author);
-
-                wiseSayings[wiseSayingSize] = wiseSaying;
-                wiseSayingSize++;
-
-                //System.out.println(Arrays.toString(wiseSayings)); // 그냥외우기 배열을 오버라이드하는법
-
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+                actionAdd() ;
             } else if (cmd.equals("목록")) {
                 System.out.println("번호/ 작가 / 명언");
                 System.out.println("-----------------------");
@@ -50,6 +36,23 @@ class App {
             }
         }
         scanner.close();
+    }
+
+    void actionAdd() {
+        System.out.println("명언 : ");
+        String content = scanner.nextLine();
+        System.out.println("작가 : ");
+        String author = scanner.nextLine();
+        int id = ++lastId;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+
+        wiseSayings[wiseSayingSize] = wiseSaying;
+        wiseSayingSize++;
+
+        //System.out.println(Arrays.toString(wiseSayings)); // 그냥외우기 배열을 오버라이드하는법
+
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
     }
 }
 
